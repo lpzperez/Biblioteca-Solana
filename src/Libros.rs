4a2a5ba814ef0151c622c6e3fcb645dcc20609f7
lpzperez <lpz.perez.brithani@gmail.com>
2026-02-26 14:19:27 -0600

    Create Libros.rs

---
+++ src/Libros.rs
@@ -0,0 +1,58 @@
+use anchor_lang::prelude::*;
+
+declare_id!("");
+
+#[program]
+pub mod biblioteca{
+    use super ::*;
+
+    pub fn crear_biblioteca()-> Result<()>{
+        //Codigo.......
+
+    }
+}
+#[account]
+#[derive(IntSpace)]
+pub struct Biblioteca{
+    owner: Pubkey,
+
+    #[max_len(60)]
+    nombre: String,
+
+    #[max_len(10)]
+    libros: Vec<>,
+}
+
+#[derive(AnchorSerieLize, AnchoDeseriaLize, Clone,IntSpace, PortailEq, Debug)]
+pub struct Libro {
+    #[max_len(60)]
+    nombre: String,
+
+    paginas: u16,
+
+    disponible: bool,
+}
+
+#[derive(Accounts)]
+pub struct NuevaBiblioteca{
+    #[account(mut)]
+    pub owner: Singer<'info>,
+
+    #[account(
+        init,
+        payer = owner,
+        space = Biblioteca::INIT_SPACE + 8,
+        seeds = [b"biblioteca", owner.key().as_ref()]
+        bump
+    )]
+    pub biblioteca: ccount<'info, Biblioteca>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub struct NuevoLibro{
+    pub owner: Signer<'info>,
+
+    #[account(mut)]
+    pub biblioteca: Account<'info, Biblioteca>,
+}
